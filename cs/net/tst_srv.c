f8a8106712b9fb2307a0f6cfd1ecd420ebaf3d39
hans.opva <hans@opva.com>
2011-06-02 14:03:56 +0800

    add the poll version

--- cs/net/tst_srv.c
+++ cs/net/tst_srv.c
@@ -24,9 +24,13 @@
 #include <strings.h>
 #include <string.h>
 
+#include <poll.h>
+#include <limits.h>
+
 #include <arpa/inet.h>
 
 #define MAXLINE 1024
+#define OPENMAX 1024
 
 void read2write(int fd)
 {
@@ -40,6 +44,52 @@ void read2write(int fd)
     }
 
     return;
+}
+
+void add_client_fd(struct pollfd *clients, int fd, int *pindex)
+{
+    int i = 0;
+
+    for(; i < *pindex; i++)
+    {
+        if(clients[i].fd == -1)
+        {
+            clients[i].fd = fd;
+            clients[i].events = POLLIN;
+            break;
+        }
+    }
+
+    if(i == *pindex)
+    {
+        *pindex += 1;
+        clients[*pindex].fd = fd;
+        clients[*pindex].events = POLLIN;
+    }
+
+    return;
+}
+
+int pread2write(int fd)
+{
+    char buf[MAXLINE];
+    memset(buf, '\0', sizeof(buf));
+
+    int len = read(fd, buf, sizeof(buf));
+    if(len == 0)
+    {
+        printf("end of data\n");
+        return 1;
+    } else if(len > 0)
+    {
+        write(fd, buf, len);
+    } else
+    {
+        perror("read error");
+        return -1;
+    }
+
+    return 0;
 }
 
 int main()
@@ -60,6 +110,54 @@ int main()
     bind(fd, (struct sockaddr*)&saddr, sizeof(saddr));
     listen(fd, 5);
 
+    struct pollfd clients[OPENMAX];
+    int i, index, retval;
+
+    clients[0].fd = fd;
+    clients[0].events = POLLIN;
+    index = 0;
+
+    /* initial client array */
+    for(i = 1; i < OPENMAX; i++)
+        clients[i].fd = -1;
+
+    while(1)
+    {
+        retval = poll(clients, index + 1, -1); /* infinite timeout */
+
+        if(retval > 0)
+        {
+            if(clients[0].revents & POLLIN) /* accept new connection */
+            {
+                len = sizeof(caddr);
+                cfd = accept(clients[0].fd, (struct sockaddr*)&caddr, &len);
+
+                str_caddr = inet_ntoa(caddr.sin_addr);
+                printf("connection from %s, port %d\n", str_caddr, ntohs(caddr.sin_port));
+                if(cfd <= 0)
+                    perror("accept error");
+                else
+                    add_client_fd(clients, cfd, &index);
+            }
+
+            for(i = 1; i <= index; i++)
+            {
+                if(clients[i].fd < 0)
+                    continue;
+                if(clients[i].revents & POLLIN)
+                {
+                    retval = pread2write(clients[i].fd);
+                    if(retval == 1) /* client socket closed */
+                    {
+                        close(clients[i].fd);
+                        clients[i].fd = -1;
+                    }
+                }
+            }
+        }
+    }
+
+    /*
     while(1)
     {
         len = sizeof(caddr);
@@ -84,6 +182,7 @@ int main()
             exit(0);
         }
     }
+    */
 
     close(fd);
     exit(0);
