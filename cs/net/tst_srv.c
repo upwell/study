c186e55c90826d0655ce57c56f5f4a8ca0f3ec95
hans.opva <hans@opva.com>
2011-06-02 17:00:59 +0800

    initial the libevent version

--- cs/net/tst_srv.c
+++ cs/net/tst_srv.c
@@ -26,6 +26,8 @@
 
 #include <arpa/inet.h>
 
+#include <event.h>
+
 #define MAXLINE 1024
 
 void read2write(int fd)
@@ -42,12 +44,60 @@ void read2write(int fd)
     return;
 }
 
+void handle_client(int fd, short event, void* args)
+{
+    char buf[MAXLINE];
+    memset(buf, '\0', sizeof(buf));
+    int len;
+
+    if(event & EV_READ)
+    {
+        len = read(fd, buf, sizeof(buf));
+        if(len > 0)
+        {
+            write(fd, buf, len);
+        } else if(len == 0)
+        {
+            printf("end of data\n");
+            close(fd);
+            event_del((struct event*)args); /* this is a must */
+            free(args);
+        }
+    }
+
+}
+
+void conn_accept(int fd, short event, void *args)
+{
+    printf("inside conn accept [%d], event [%d]\n", fd, event);
+
+    struct sockaddr_in caddr;
+    socklen_t len;
+    char *str_caddr = NULL;
+    int cfd;
+
+    len = sizeof(caddr);
+    cfd = accept(fd, (struct sockaddr*) &caddr, &len);
+
+    str_caddr = inet_ntoa(caddr.sin_addr);
+    if(!str_caddr)
+    {
+        perror("inet_ntoa failed\n");
+    }
+    printf("connection from %s, port %d\n", str_caddr, ntohs(caddr.sin_port));
+
+    /* add the client fd into the interested list */
+    struct event *pev = (struct event*) malloc(sizeof(struct event));
+    event_set(pev, cfd, EV_READ|EV_PERSIST, handle_client, pev);
+    event_add(pev, NULL);
+
+    return;
+}
+
 int main()
 {
     int fd = socket(AF_INET, SOCK_STREAM, 0);
-    struct sockaddr_in saddr, caddr;
-    socklen_t len;
-    char *str_caddr = NULL;
+    struct sockaddr_in saddr;
     pid_t cid;
     int cfd;
     char data[] = "abcd";
@@ -60,6 +110,14 @@ int main()
     bind(fd, (struct sockaddr*)&saddr, sizeof(saddr));
     listen(fd, 5);
 
+    event_init();
+    struct event ev;
+    event_set(&ev, fd, EV_READ | EV_PERSIST, conn_accept, &ev);
+
+    event_add(&ev, NULL);
+    event_dispatch();
+
+    /*
     while(1)
     {
         len = sizeof(caddr);
@@ -84,6 +142,7 @@ int main()
             exit(0);
         }
     }
+    */
 
     close(fd);
     exit(0);
