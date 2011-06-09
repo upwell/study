cb8362153298bf6344ea6ee76451548df49bbbe0
Han He <upwell@gmail.com>
2011-06-10 00:45:07 +0800

    add the login session part

--- cs/net/tst_srv.js
+++ cs/net/tst_srv.js
@@ -2,22 +2,34 @@ var net = require('net');
 
 var socks = new Array();
 
-function notifyAllClients(data)
+function notifyAllClients(data, loginName)
 {
     for(var i = 0; i < socks.length; i++)
     {
-        socks[i].write(data + '> ');
+        if(loginName != socks[i][0])
+            socks[i][1].write(data);
     }
-}
+} var srv = net.createServer(function(stream) { console.log('connection is accepted');
+    stream.write("welcome, please enter your name:");
 
-var srv = net.createServer(function(stream) {
-    console.log('connection is accepted');
-    stream.write("welcome\r\n");
-
-    socks.push(stream); /* add to global socks array */
+    var isName = true;
+    var loginName = "";
 
     stream.on('data', function(data) {
-        notifyAllClients(data);
+        if(isName)
+        {
+            var sockNode = new Array();
+            data = new String(data).trim();
+            loginName = data;
+            sockNode[0] = data;
+            sockNode[1] = stream;
+            socks.push(sockNode);
+            isName = false;
+        } else
+        {
+            data = loginName + " said : " + data;
+            notifyAllClients(data, loginName);
+        }
     });
 }
 );
