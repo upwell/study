2aeddb63627e7b845604e9954cdd6ef273075a2e
Han He <upwell@gmail.com>
2011-07-06 17:09:54 +0800

    libev version

--- cs/net/tst_srv.c
+++ cs/net/tst_srv.c
@@ -26,58 +26,41 @@
 
 #include <arpa/inet.h>
 
-#include <event.h>
+#include <ev.h>
 
 #define MAXLINE 1024
 
-void read2write(int fd)
-{
-    char buf[MAXLINE];
-    memset(buf, '\0', sizeof(buf));
-    int len;
-    while((len = read(fd, buf, sizeof(buf))) > 0)
-    {
-        write(fd, buf, len);
-        memset(buf, '\0', sizeof(buf));
-    }
-
-    return;
-}
-
-void handle_client(int fd, short event, void* args)
+static void handle_client(struct ev_loop *loop, ev_io *w, int revents)
 {
     char buf[MAXLINE];
     memset(buf, '\0', sizeof(buf));
     int len;
 
-    if(event & EV_READ)
+    if(revents & EV_READ)
     {
-        len = read(fd, buf, sizeof(buf));
+        len = read(w->fd, buf, sizeof(buf));
         if(len > 0)
         {
-            write(fd, buf, len);
+            write(w->fd, buf, len);
         } else if(len == 0)
         {
             printf("end of data\n");
-            close(fd);
-            event_del((struct event*)args); /* this is a must */
-            free(args);
+            close(w->fd);
+            ev_io_stop(loop, w);
+            free(w);
         }
     }
-
 }
 
-void conn_accept(int fd, short event, void *args)
+static void conn_accept(struct ev_loop *loop, ev_io *w, int revents)
 {
-    printf("inside conn accept [%d], event [%d]\n", fd, event);
-
     struct sockaddr_in caddr;
     socklen_t len;
     char *str_caddr = NULL;
     int cfd;
 
     len = sizeof(caddr);
-    cfd = accept(fd, (struct sockaddr*) &caddr, &len);
+    cfd = accept(w->fd, (struct sockaddr*) &caddr, &len);
 
     str_caddr = inet_ntoa(caddr.sin_addr);
     if(!str_caddr)
@@ -87,9 +70,9 @@ void conn_accept(int fd, short event, void *args)
     printf("connection from %s, port %d\n", str_caddr, ntohs(caddr.sin_port));
 
     /* add the client fd into the interested list */
-    struct event *pev = (struct event*) malloc(sizeof(struct event));
-    event_set(pev, cfd, EV_READ|EV_PERSIST, handle_client, pev);
-    event_add(pev, NULL);
+    ev_io *pev = (ev_io*) malloc(sizeof(ev_io));
+    ev_io_init(pev, handle_client, cfd, EV_READ);
+    ev_io_start(loop, pev);
 
     return;
 }
@@ -110,39 +93,12 @@ int main()
     bind(fd, (struct sockaddr*)&saddr, sizeof(saddr));
     listen(fd, 5);
 
-    event_init();
-    struct event ev;
-    event_set(&ev, fd, EV_READ | EV_PERSIST, conn_accept, &ev);
+    struct ev_loop *loop = ev_default_loop(0);
+    ev_io listener;
+    ev_io_init(&listener, conn_accept, fd, EV_READ);
+    ev_io_start(loop, &listener);
 
-    event_add(&ev, NULL);
-    event_dispatch();
-
-    /*
-    while(1)
-    {
-        len = sizeof(caddr);
-        cfd = accept(fd, (struct sockaddr*)&caddr, &len);
-        if(cfd < 0)
-            perror("accept error");
-        if( (cid = fork()) > 0 )
-        {
-            continue;
-        } else
-        {
-            close(fd);
-
-            str_caddr = inet_ntoa(caddr.sin_addr);
-            if(!str_caddr)
-            {
-                perror("inet_ntoa failed\n");
-            }
-            printf("connection from %s, port %d\n", str_caddr, ntohs(caddr.sin_port));
-            read2write(cfd);
-            close(cfd);
-            exit(0);
-        }
-    }
-    */
+    ev_run(loop, 0);
 
     close(fd);
     exit(0);
