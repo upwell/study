244d46975b1300939c6f008db81092456120e18d
Han He <han@he>
2012-09-28 16:35:18 +0800

    update example.html

---
+++ webui/js/flot/jquery.flot.barnumbers.js
@@ -0,0 +1,69 @@
+/* Copyright Steel Business Briefing, FreeBSD-License
+ * simple flot plugin to draw bar numbers halfway in bars
+ *
+ * options are
+ * series: {
+ *     bars: {
+ *         showNumbers: boolean
+ *     }
+ * }
+ *
+ * Add the support of horizontal bar
+ */
+(function ($) {
+    var options = {
+        bars: { showNumbers: false }
+    };
+
+    function init(plot) {
+        function drawNumbers(plot, ctx, series){
+            if(series.bars.showNumbers){
+                if(series.bars.horizontal){
+                    var points = series.datapoints.points
+                    var ctx = plot.getCanvas().getContext('2d');
+                    var offset = plot.getPlotOffset();
+                    ctx.textBaseline = "top";
+                    ctx.textAlign = "center";
+                    if(series.bars.align == "left")
+                        alignOffset = series.bars.barWidth / 2;
+                    else
+                        alignOffset = 0;
+
+                    for(var datapoint in points){
+                        if((datapoint - 1) % 3 == 0){
+                            var point = { 'x': points[datapoint - 1] / 2, 'y': points[datapoint] + alignOffset}
+                            var c = plot.p2c(point);
+                            ctx.fillText(points[datapoint].toString(10), c.left + offset.left, c.top + offset.top)
+                        }
+                    }
+                } else {
+                    var points = series.datapoints.points
+                    var ctx = plot.getCanvas().getContext('2d');
+                    var offset = plot.getPlotOffset();
+                    ctx.textBaseline = "top";
+                    ctx.textAlign = "center";
+                    if(series.bars.align == "left")
+                        alignOffset = series.bars.barWidth / 2;
+                    else
+                        alignOffset = 0;
+
+                    for(var datapoint in points){
+                        if((datapoint - 1) % 3 == 0){
+                            var point = { 'x': points[datapoint - 1] + alignOffset, 'y': points[datapoint] / 2 }
+                            var c = plot.p2c(point);
+                            ctx.fillText(points[datapoint].toString(10), c.left + offset.left, c.top + offset.top)
+                        }
+                    }
+                }
+            }
+        }
+        plot.hooks.drawSeries.push(drawNumbers);
+    }
+
+    $.plot.plugins.push({
+        init: init,
+        options: options,
+        name: 'barnumbers',
+        version: '0.1'
+    });
+})(jQuery);
